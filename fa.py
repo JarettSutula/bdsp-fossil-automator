bafa06691d2a2b23c51f8744d35e13a3f9bccec1
JarettSutula <Jarett.Sutula1@marist.edu>
2023-01-27 17:45:33 -0500

    webapp working, start of fossil drop script

---
+++ fa.py
@@ -0,0 +1,31 @@
+launch_title = """
+A 0.1s
+2.0s
+A 0.1s
+30.0s
+A 0.1s
+3.0s
+A 0.1s
+"""
+
+one_fossil_cycle = """
+DPAD_UP 0.5s
+3.0s
+L_STICK@+000+100 1.5s
+DPAD_RIGHT 0.3s
+DPAD_UP 0.1s
+A 0.1s
+1.0s
+A 0.1s
+1.0s
+A 0.1s
+1.0s
+A 0.1s
+1.0s
+A 0.1s
+1.0s
+A 0.1s
+1.0s
+A 0.1s
+1.0s
+"""
